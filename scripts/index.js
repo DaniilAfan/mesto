9b2beeaa3ab3e652cffa6d68dd7f34ce3a836803
DaniilAfan <110550038+DaniilAfan@users.noreply.github.com>
2023-05-10 22:21:15 +0300

    на проверку 3

--- scripts/index.js
+++ scripts/index.js
@@ -9,8 +9,6 @@ const profilejob = document.querySelector('.profile__subtitle');
 const submitButton = document.querySelector('.popup__save-button_profile');
 //открытие попапа профиля
 const togglePopupVisibility = function (popup) {
-    nameInput.value = profilename.textContent;
-    jobInput.value = profilejob.textContent;
     popup.classList.add('popup_opened');
 }
 //закрытие попапа профиля
@@ -19,6 +17,8 @@ const togglePopupInVisibility = function (popup) {
 }
 
 profilepopupElement.addEventListener('click', () => {
+    nameInput.value = profilename.textContent;
+    jobInput.value = profilejob.textContent;
     togglePopupVisibility(popupElement);
 });
 popupcloseElement.addEventListener('click', () => {
@@ -32,7 +32,7 @@ function handleFormSubmitProfile(event) {
     profilejob.textContent = jobInput.value;
     togglePopupInVisibility(popupElement);
 }
-popupForm.addEventListener('submit', handleFormSubmit);
+popupForm.addEventListener('submit', handleFormSubmitProfile);
 //объявляем переменные для второго попапа
 const profileAddbutton = document.querySelector('.profile__button-add');
 const popupLink = document.querySelector('.popup__input_type_link');
@@ -40,10 +40,10 @@ const popupName = document.querySelector('.popup__input_type_title');
 const popupCards = document.querySelector('.popup_cards')
 const popupclosebutton = document.querySelector('.popup__close-button_cards');
 //объявляем третий попап
-const popupFullScreen = document.querySelector('.popup-fullscreen');
-const popupFullScreenImg = document.querySelector('.popup-fullscreen__img');
-const popupFullScreenTitle = document.querySelector('.popup-fullscreen__title');
-const popupFullScreenClose = document.querySelector('.popup-fullscreen__close');
+const popupFullScreen = document.querySelector('.popup_fullscreen');
+const popupFullScreenImg = document.querySelector('.popup__fullscreen-img');
+const popupFullScreenTitle = document.querySelector('.popup__fullscreen-title');
+const popupFullScreenClose = document.querySelector('.popup__fullscreen-close');
 //открытие попапа карточек
 profileAddbutton.addEventListener('click', () => {
     togglePopupVisibility(popupCards);
@@ -83,12 +83,12 @@ function createCard(item) {
         togglePopupVisibility(popupFullScreen);
     }
     cardLogo.addEventListener('click', openFullScreen);
-    //закрыть
-    popupFullScreenClose.addEventListener('click', () => {
-        togglePopupInVisibility(popupFullScreen);
-    });
     return templateElement;
 }
+//закрыть попап большой картинки
+popupFullScreenClose.addEventListener('click', () => {
+    togglePopupInVisibility(popupFullScreen);
+});
 initialCards.forEach(function (item) {
     const card = createCard(item);
     cards.prepend(card);
